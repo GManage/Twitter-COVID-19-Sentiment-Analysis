7022757a93c8d11155e82190152d4e36b57ed678
Christopher Bach <chrabach@gmail.com>
2021-09-19 20:55:44 -0400

    regression analysis

---
+++ 03.Machine_Learning/27.RRegression.r
@@ -0,0 +1,30 @@
+
+library(dplyr)
+path = "C:\\Users\\Christopher\\OneDrive\\Desktop\\Assignments\\PowerRangersSandbox\\"
+
+#file = "agg_sentiment_w3classifiers_olddata.csv"
+file = "agg_sentiment_w3classifiers_olddata_wNLTK.csv"
+
+filepath = paste(path , file, sep="")
+
+classifiers <- read.csv(filepath,check.names=F,stringsAsFactors = F)
+
+file = "vaccinationbydate.csv"
+filepath = paste(path , file, sep="")
+
+administered <- read.csv(filepath,check.names=F,stringsAsFactors = F)
+
+tweetdata <- merge (classifiers, administered, by="normalized_date")
+
+Administered <- tweetdata$Administered
+textblob_agg_sentiment <- tweetdata$textblob_agg_sentiment
+nltk_agg_sentiment <- tweetdata$nltk_agg_sentiment
+vader_agg_sentiment <-  tweetdata$Vader_agg_sentiment
+vader_agg_sentiment
+
+
+fit <- lm(Administered ~ textblob_agg_sentiment + nltk_agg_sentiment + vader_agg_sentiment, date = tweetdata)
+fit <- lm(Administered ~ nltk_agg_sentiment, date = tweetdata)
+
+summary(fit)
+
